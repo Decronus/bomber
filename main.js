0420897e6b4f8239ea70bfad54fad6be580b158b
Decronus <zick3333@mail.ru>
2022-11-14 07:38:46 +0100

    final

---
+++ main.js
@@ -0,0 +1,280 @@
+function openPopupCons() {
+    var popupCons = document.getElementById('popup-cons-back');
+    var popupContainerCons = document.getElementById('popup-container-cons');
+
+    popupCons.style.opacity = "100%";
+    popupCons.style.visibility = "visible";
+    
+    popupContainerCons.style.opacity = "100%";
+    popupContainerCons.style.transform = "translate(0px, 0)";
+}
+
+function closePopupCons() {
+    var popupCons = document.getElementById('popup-cons-back');
+    var popupContainerCons = document.getElementById('popup-container-cons');
+
+    popupCons.style.opacity = "0";
+    popupCons.style.visibility = "hidden";
+
+    popupContainerCons.style.opacity = "0";
+    popupContainerCons.style.transform = "translate(0px, -100%)";
+}
+
+//ТАРГЕТ
+function popupTarget() {
+    document.getElementById('popup-target-back').style.opacity = "100%";
+    document.getElementById('popup-target-back').style.visibility = "visible";
+    
+    document.getElementById('popup-target').style.opacity = "100%";
+    document.getElementById('popup-target').style.transform = "translate(0px, 0)";
+}
+
+function closePopupTarget() {
+    document.getElementById('popup-target-back').style.opacity = "0";
+    document.getElementById('popup-target-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-target').style.opacity = "0";
+    document.getElementById('popup-target').style.transform = "translate(0px, -100%)";
+}
+
+//КОНТЕКСТ
+function popupCont() {
+    document.getElementById('popup-cont-back').style.opacity = "100%";
+    document.getElementById('popup-cont-back').style.visibility = "visible";
+    
+    document.getElementById('popup-cont').style.opacity = "100%";
+    document.getElementById('popup-cont').style.transform = "translate(0px, 0)";
+}
+
+function closePopupCont() {
+    document.getElementById('popup-cont-back').style.opacity = "0";
+    document.getElementById('popup-cont-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-cont').style.opacity = "0";
+    document.getElementById('popup-cont').style.transform = "translate(0px, -100%)";
+}
+
+//SMM
+function popupSMM() {
+    document.getElementById('popup-smm-back').style.opacity = "100%";
+    document.getElementById('popup-smm-back').style.visibility = "visible";
+    
+    document.getElementById('popup-smm').style.opacity = "100%";
+    document.getElementById('popup-smm').style.transform = "translate(0px, 0)";
+}
+
+function closePopupSMM() {
+    document.getElementById('popup-smm-back').style.opacity = "0";
+    document.getElementById('popup-smm-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-smm').style.opacity = "0";
+    document.getElementById('popup-smm').style.transform = "translate(0px, -100%)";
+}
+
+//DESIGN
+function popupDesign() {
+    document.getElementById('popup-design-back').style.opacity = "100%";
+    document.getElementById('popup-design-back').style.visibility = "visible";
+    
+    document.getElementById('popup-design').style.opacity = "100%";
+    document.getElementById('popup-design').style.transform = "translate(0px, 0)";
+}
+
+function closePopupDesign() {
+    document.getElementById('popup-design-back').style.opacity = "0";
+    document.getElementById('popup-design-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-design').style.opacity = "0";
+    document.getElementById('popup-design').style.transform = "translate(0px, -100%)";
+}
+
+//LANDING
+function popupLanding() {
+    document.getElementById('popup-landing-back').style.opacity = "100%";
+    document.getElementById('popup-landing-back').style.visibility = "visible";
+    
+    document.getElementById('popup-landing').style.opacity = "100%";
+    document.getElementById('popup-landing').style.transform = "translate(0px, 0)";
+}
+
+function closePopupLanding() {
+    document.getElementById('popup-landing-back').style.opacity = "0";
+    document.getElementById('popup-landing-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-landing').style.opacity = "0";
+    document.getElementById('popup-landing').style.transform = "translate(0px, -100%)";
+}
+
+//CRM
+function popupCRM() {
+    document.getElementById('popup-crm-back').style.opacity = "100%";
+    document.getElementById('popup-crm-back').style.visibility = "visible";
+    
+    document.getElementById('popup-crm').style.opacity = "100%";
+    document.getElementById('popup-crm').style.transform = "translate(0px, 0)";
+}
+
+function closePopupCRM() {
+    document.getElementById('popup-crm-back').style.opacity = "0";
+    document.getElementById('popup-crm-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-crm').style.opacity = "0";
+    document.getElementById('popup-crm').style.transform = "translate(0px, -100%)";
+}
+
+//LIGHT
+function popupLight() {
+    document.getElementById('popup-light-back').style.opacity = "100%";
+    document.getElementById('popup-light-back').style.visibility = "visible";
+    
+    document.getElementById('popup-light').style.opacity = "100%";
+    document.getElementById('popup-light').style.transform = "translate(0px, 0)";
+}
+
+function closePopupLight() {
+    document.getElementById('popup-light-back').style.opacity = "0";
+    document.getElementById('popup-light-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-light').style.opacity = "0";
+    document.getElementById('popup-light').style.transform = "translate(0px, -100%)";
+}
+
+//OPTIM
+function popupOptim() {
+    document.getElementById('popup-optim-back').style.opacity = "100%";
+    document.getElementById('popup-optim-back').style.visibility = "visible";
+    
+    document.getElementById('popup-optim').style.opacity = "100%";
+    document.getElementById('popup-optim').style.transform = "translate(0px, 0)";
+}
+
+function closePopupOptim() {
+    document.getElementById('popup-optim-back').style.opacity = "0";
+    document.getElementById('popup-optim-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-optim').style.opacity = "0";
+    document.getElementById('popup-optim').style.transform = "translate(0px, -100%)";
+}
+
+//BOMB
+function popupBomb() {
+    document.getElementById('popup-bomb-back').style.opacity = "100%";
+    document.getElementById('popup-bomb-back').style.visibility = "visible";
+    
+    document.getElementById('popup-bomb').style.opacity = "100%";
+    document.getElementById('popup-bomb').style.transform = "translate(0px, 0)";
+}
+
+function closePopupBomb() {
+    document.getElementById('popup-bomb-back').style.opacity = "0";
+    document.getElementById('popup-bomb-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-bomb').style.opacity = "0";
+    document.getElementById('popup-bomb').style.transform = "translate(0px, -100%)";
+}
+
+//РАСЧЁТ
+function popupCalc() {
+    document.getElementById('popup-calc-back').style.opacity = "100%";
+    document.getElementById('popup-calc-back').style.visibility = "visible";
+    
+    document.getElementById('popup-calc').style.opacity = "100%";
+    document.getElementById('popup-calc').style.transform = "translate(0px, 0)";
+}
+
+function closePopupCalc() {
+    document.getElementById('popup-calc-back').style.opacity = "0";
+    document.getElementById('popup-calc-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-calc').style.opacity = "0";
+    document.getElementById('popup-calc').style.transform = "translate(0px, -100%)";
+}
+
+//КОНСУЛЬТАЦИЯ ЧЁРНАЯ ФОРМА
+function popupBlackCons() {
+    document.getElementById('popup-black-cons-back').style.opacity = "100%";
+    document.getElementById('popup-black-cons-back').style.visibility = "visible";
+    
+    document.getElementById('popup-black-cons').style.opacity = "100%";
+    document.getElementById('popup-black-cons').style.transform = "translate(0px, 0)";
+}
+
+function closeBlackCons() {
+    document.getElementById('popup-black-cons-back').style.opacity = "0";
+    document.getElementById('popup-black-cons-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-black-cons').style.opacity = "0";
+    document.getElementById('popup-black-cons').style.transform = "translate(0px, -100%)";
+}
+
+//БУРГЕР
+function popupBurger() {
+    document.getElementById('popup-burger-back').style.opacity = "100%";
+    document.getElementById('popup-burger-back').style.visibility = "visible";
+    
+    document.getElementById('popup-burger').style.opacity = "100%";
+    document.getElementById('popup-burger').style.transform = "translate(0px, 0)";
+}
+
+function closePopupBurger() {
+    document.getElementById('popup-burger-back').style.opacity = "0";
+    document.getElementById('popup-burger-back').style.visibility = "hidden";
+    
+    document.getElementById('popup-burger').style.opacity = "0";
+    document.getElementById('popup-burger').style.transform = "translate(0px, -100%)";
+}
+
+//УСПЕШНАЯ ОТПРАВКА
+function openPopupFormSuccess() {
+    var popupFormSuccessBack = document.getElementById('popup-form-success-back');
+    var popupFormSuccess = document.getElementById('popup-form-success');
+
+    popupFormSuccessBack.style.opacity = "100%";
+    popupFormSuccessBack.style.visibility = "visible";
+    
+    popupFormSuccess.style.opacity = "100%";
+    popupFormSuccess.style.transform = "translate(0px, 0)";
+}
+
+function backToMainPage() {
+    window.location = 'onlinebomber.ru'
+}
+
+
+
+$(document).ready(function () {
+    $('.telegram-form').on('submit', function (event) {
+
+        event.stopPropagation();
+        event.preventDefault();
+
+        let form = this,
+            submit = $('.submit', form),
+            data = new FormData(),
+            files = $('input[type=file]');
+
+        data.append('name',  $('[name="name"]', form).val() );
+        data.append('phone', $('[name="phone"]', form).val() );
+
+        $('.submit', form).val('Отправка...');
+        $('input, textarea', form).attr('disabled','');
+        
+        $.ajax({
+            url: 'ajax.php',
+            type: 'POST',
+            data: data,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            error: function( jqXHR, textStatus ) {
+                // dfasd
+            },
+            complete: function() {
+                // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+                console.log('Complete')
+                form.reset()
+                openPopupFormSuccess()
+            }
+        });
+    });
+});
